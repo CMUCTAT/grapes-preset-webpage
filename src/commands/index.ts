f1e5ad6e7c339fde1a932c45617e8be32e5c53f0
Prasanna <ppantha28@gmail.com>
2024-11-15 21:22:23 -0500

    reformatting

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,12 +1,15 @@
 import type { Editor } from 'grapesjs';
+// import CommandObject from 'grapesjs';
 import { RequiredPluginOptions } from '..';
 import {
   cmdClear,
   cmdDeviceDesktop,
   cmdDeviceMobile,
   cmdDeviceTablet,
+  TARGET_PANEL_ID,
 } from './../consts';
 import openImport from './openImport';
+
 
 export default (editor: Editor, config: RequiredPluginOptions) => {
   const { Commands } = editor;
@@ -27,4 +30,203 @@ export default (editor: Editor, config: RequiredPluginOptions) => {
     stop: () => {},
   });
   Commands.add(cmdClear, (e: Editor) => confirm(txtConfirm) && e.runCommand('core:canvas-clear'));
+
+  Commands.add('oba-override', (function () {
+      let layers: HTMLElement | null = null;
+      return {
+  
+        run(editor: any) {
+  
+          const lm = editor.BlockManager;
+          const pn = editor.Panels;
+          const lmConfig = lm.getConfig();
+  
+  
+          let xxx = pn.getPanel("oba-custom-panel");
+
+          if (!layers) {
+            // Create the layers container
+            const layersElement = document.createElement('div');
+            layersElement.id = "oba-custom-panel"; // Optional: Assign an ID for easier debugging
+  
+            let panel = pn.getPanel("oba-custom-panel");
+            if (!panel) {
+              panel = pn.addPanel({
+                id: "oba-custom-panel",
+              });
+            }
+  
+            if (lmConfig.custom) {
+              lm.__trgCustom({ container: layersElement });
+            } else {
+              layersElement.appendChild(lm.render());
+            }
+  
+            panel.set('appendContent', layersElement).trigger('change:appendContent');
+            layers = layersElement;
+          }
+  
+          layers.style.display = 'block';
+        },
+        stop() {
+          if (layers) {
+            layers.style.display = 'none';
+          }
+        },
+      };
+    })());
+
+  Commands.add('ota-override', (function () {
+      let layers: HTMLElement | null = null;
+      return {
+  
+        run(editor: any) {
+  
+          const lm = editor.TraitManager;
+          const pn = editor.Panels;
+          const lmConfig = lm.getConfig();
+  
+  
+          let xxx = pn.getPanel('custom-ota-panel');
+          console.log("panel");
+          console.log(xxx);
+  
+          if (lmConfig.appendTo) return;
+
+          if (!layers) {
+            // Create the layers container
+            const layersElement = document.createElement('div');
+            layersElement.id = 'custom-ota-panel'; // Optional: Assign an ID for easier debugging
+  
+            let panel = pn.getPanel('custom-ota-panel');
+            if (!panel) {
+              panel = pn.addPanel({
+                id: 'custom-ota-panel',
+              });
+            }
+  
+            if (lmConfig.custom) {
+              lm.__trgCustom({ container: layersElement });
+            } else {
+              layersElement.appendChild(lm.render());
+            }
+  
+            panel.set('appendContent', layersElement).trigger('change:appendContent');
+            layers = layersElement;
+          }
+  
+          layers.style.display = 'block';
+        },
+        stop() {
+        },
+      };
+    })());
+  
+  Commands.add('osa-override', (function () {
+      let layers: HTMLElement | null = null;
+      return {
+  
+        run(editor: any) {
+  
+          const lm = editor.StyleManager;
+          const pn = editor.Panels;
+          const lmConfig = lm.getConfig();
+  
+  
+          let xxx = pn.getPanel('custom-osa-panel');
+          console.log("panel");
+          console.log(xxx);
+  
+          if (lmConfig.appendTo) return;
+          if (!layers) {
+            const layersElement = document.createElement('div');
+            layersElement.id = 'custom-osa-panel';
+  
+            let panel = pn.getPanel('custom-osa-panel');
+            if (!panel) {
+              panel = pn.addPanel({
+                id: 'custom-osa-panel',
+              });
+            }
+  
+            if (lmConfig.custom) {
+              lm.__trgCustom({ container: layersElement });
+            } else {
+              layersElement.appendChild(lm.render());
+            }
+  
+            panel.set('appendContent', layersElement).trigger('change:appendContent');
+            layers = layersElement;
+          }
+  
+          layers.style.display = 'block';
+        },
+        stop() {
+
+        },
+      };
+    })());
+  
+  
+  editor.Commands.add('ola-override', (function () {
+    let layers: HTMLElement | null = null;
+    return {
+
+      run(editor: any) {
+
+        const lm = editor.LayerManager;
+        const pn = editor.Panels;
+        const lmConfig = lm.getConfig();
+
+
+        let xxx = pn.getPanel(TARGET_PANEL_ID);
+        console.log("panel");
+        console.log(xxx);
+
+        if (lmConfig.appendTo) return;
+        const wrapper = editor.getWrapper();
+        console.log(wrapper)
+        if(wrapper && editor.getDevice() == 'Desktop'){
+        wrapper.setStyle({
+          padding:'0px 0px 0px 200px'
+        });
+      }
+        if (!layers) {
+          // Create the layers container
+          const layersElement = document.createElement('div');
+          layersElement.id = 'my-custom-layers'; // Optional: Assign an ID for easier debugging
+
+          let panel = pn.getPanel(TARGET_PANEL_ID);
+          if (!panel) {
+            panel = pn.addPanel({
+              id: TARGET_PANEL_ID,
+            });
+          }
+
+          if (lmConfig.custom) {
+            lm.__trgCustom({ container: layersElement });
+          } else {
+            layersElement.appendChild(lm.render());
+          }
+
+          panel.set('appendContent', layersElement).trigger('change:appendContent');
+          layers = layersElement;
+        }
+
+        layers.style.display = 'block';
+      },
+      stop() {
+        if (layers) {
+          layers.style.display = 'none';
+          const wrapper = editor.getWrapper();
+          if(wrapper)
+          {
+          wrapper.setStyle({
+            padding:'0px 0px 0px 0px'
+          });
+        }
+        }
+      },
+    };
+  })());
 }
--- src/panels.ts
+++ src/panels.ts
@@ -6,7 +6,8 @@ import {
   cmdDeviceDesktop,
   cmdDeviceTablet,
   cmdDeviceMobile,
-  cmdClear
+  cmdClear,
+  TARGET_PANEL_ID,
 } from './consts';
 
 export default (editor: Editor, opts: RequiredPluginOptions) => {
@@ -29,9 +30,53 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
       id: 'commands',
       buttons: [{}],
     },
+
     {
       id: 'devices-c',
       buttons: [{
+      id: ola,
+      command: "ola-override",
+      label:
+      `
+<div 
+  class="layers-button-content" 
+  style="
+    width: 180px; 
+    height: 15px; 
+    display: flex;
+    justify-content: flex-start;
+  "
+  title="Layers Panel"
+>
+  <svg 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    style="
+      fill: white; 
+      transition: transform 0.3s ease;
+    "
+    class="dropdown-arrow"
+  >
+    
+    <path d="M7 10l5 5 5-5z"/>
+  </svg>
+  <span 
+    class="layers-button-text" 
+    style="
+      font-size: 14px; 
+      color: white; 
+      font-weight: 500;
+      user-select: none;
+    "
+  >
+      Layers
+  </span>
+
+</div>
+    `
+    },
+    {
         id: cmdDeviceDesktop,
         command: cmdDeviceDesktop,
         active: true,
@@ -54,7 +99,14 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
     },
     {
       id: 'options',
-      buttons: [{
+      buttons: [
+        {
+          id: 'oba-override',
+          command: 'oba-override',
+          label: `<svg ${iconStyle} viewBox="0 0 24 24">
+              <path fill="currentColor" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
+          </svg>`
+        },{
         id: swv,
         command: swv,
         context: swv,
@@ -104,46 +156,108 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
               <path fill="currentColor" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
           </svg>`,
       }],
-  },{
-    id: 'views',
-    buttons  : [{
-      id: osm,
-      command: osm,
-      active: true,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-            <path fill="currentColor" d="M20.71,4.63L19.37,3.29C19,2.9 18.35,2.9 17.96,3.29L9,12.25L11.75,15L20.71,6.04C21.1,5.65 21.1,5 20.71,4.63M7,14A3,3 0 0,0 4,17C4,18.31 2.84,19 2,19C2.92,20.22 4.5,21 6,21A4,4 0 0,0 10,17A3,3 0 0,0 7,14Z" />
-        </svg>`,
-    },{
-      id: otm,
-      command: otm,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
-      </svg>`
-    },{
-      id: ola,
-      command: ola,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M12,16L19.36,10.27L21,9L12,2L3,9L4.63,10.27M12,18.54L4.62,12.81L3,14.07L12,21.07L21,14.07L19.37,12.8L12,18.54Z" />
-      </svg>`
-    },{
-      id: obl,
-      command: obl,
-      label: `<svg ${iconStyle} viewBox="0 0 24 24">
-          <path fill="currentColor" d="M17,13H13V17H11V13H7V11H11V7H13V11H17M19,3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3Z" />
-      </svg>`
-    }],
   }]);
 
-  const openBl = Panels.getButton('views', obl);
-  editor.on('load', () => openBl?.set('active', true));
 
-  // On component change show the Style Manager
+
+  const panelEl = document.createElement('div');
+  const panelE2 = document.createElement('div');
+  const panelE3 = document.createElement('div');
+  const panelE4 =  document.createElement('div');
+  panelEl.id = TARGET_PANEL_ID;
+  panelE2.id = 'custom-osa-panel'
+  panelE3.id = 'custom-ota-panel'
+  panelE4.id = 'oba-custom-panel'
+  panelEl.className = 'my-custom-left-panel';
+  panelE2.id = 'custom-osa-panel'
+  panelE3.id = 'custom-ota-panel'
+  panelE4.id = 'oba-custom-panel'
+  document.getElementById('gjs')?.appendChild(panelEl);
+  document.getElementById('gjs')?.appendChild(panelE2);
+  document.getElementById('gjs')?.appendChild(panelE3);
+  document.getElementById('gjs')?.appendChild(panelE4);
+  const style = document.createElement('style');
+style.innerHTML = `
+.gjs-pn-active .dropdown-arrow {
+  transform: rotate(180deg);
+}
+
+.gjs-pn-oba-custom-panel
+{
+z-index: 2;
+}
+.gjs-pn-my-custom-panel
+{
+z-index: 2;
+}
+.gjs-pn-custom-ota-panel
+{
+z-index: 2;
+}
+.gjs-pn-custom-osa-panel
+{
+z-index: 2;
+}
+    #my-custom-layers {
+    position: absolute;
+    left: 0px;
+    top:40px;
+    margin-top: 0;
+    background-color: #182444;
+    width: 200px;
+    height: 100vh;
+    }
+
+    #oba-custom-panel{
+        position: absolute;
+    left: 835px;
+    top:40px;
+    margin-top: 0;
+    background-color: #182444;
+    width: 200px;
+    height: 40vh;
+    }
+
+    #custom-ota-panel{
+        position: fixed;
+        right:0px;
+    top:40px;
+    margin-top: 0;
+    background-color: #182444;
+    width: 225px;
+    height: 10vh;
+    }
+
+    #custom-osa-panel{
+        position: fixed;
+        right:0px;
+    top:20vh;
+    margin-top: 0;
+    background-color: #182444;
+    width: 225px;
+    height:85vh
+    overflow:auto;
+    }
+`
+  ;
+  document.getElementsByTagName('head')[0].appendChild(style);
+  editor.on('load', () => {
+    panelEl.style.display = 'block';
+    panelE2.style.display = 'block';
+    panelE3.style.display = 'block';
+    panelE4.style.display = 'block';
+  });
+  editor.on('load', () => {
+    editor.runCommand('osa-override');
+    editor.runCommand('ota-override');
+  });
+
+
   opts.showStylesOnChange && editor.on('component:selected', () => {
     const openSmBtn = Panels.getButton('views', osm);
     const openLayersBtn = Panels.getButton('views', ola);
+  
 
-    // Don't switch when the Layer Manager is on or
-    // there is no selected component
     if ((!openLayersBtn || !openLayersBtn.get('active')) && editor.getSelected()) {
       openSmBtn?.set('active', true);
     }
